f60b9eeab2c2313254ef8c8ae8c4d63cbc5fa56f
Mansour Moufid <mansourmoufid@gmail.com>
2018-11-13 08:32:23 -0500

    Add another summation unit test.

--- test-sum.c
+++ test-sum.c
@@ -29,5 +29,19 @@ main(void)
     sum = nu_sum(z, n);
     free(z);
     assert(nu_eq(sum, e, error));
+    const float pi = 3.14159265f;
+    struct nu_sum_state s;
+    nu_sum_init(&s);
+    n = 1000000;
+    d = 1.f;
+    float sgn = 1.f;
+    nu_sum_add(&s, 1.f);
+    for (size_t i = 1; i < n; i++) {
+        sgn *= -1.f;
+        d += 2.f;
+        nu_sum_add(&s, sgn / d);
+    }
+    sum = 4.f * nu_sum_sum(&s);
+    assert(nu_eq(sum, pi, error));
     return 0;
 }
